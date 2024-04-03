4cbfb85831ffd473fa1584142abb997d08032a50
Dineshkupendran <dineshjaikaran@gmail.com>
2024-04-03 10:13:14 +0530

    task

--- Task 3/index2.js
+++ Task 3/index2.js
@@ -6,12 +6,12 @@ xhr.onload=function() {
 //console.log("XMLHTTP request - status ", xhr.status);
 var countries = JSON.parse(xhr.responseText);
 countries.forEach(country => {
-    console.log(country.name.common + " :"+ country.flag +" :"+ country.region + " :"+ country.subregion);
+   // console.log(country.name.common + " :"+ country.flag +" :"+ country.region + " :"+ country.subregion);
   //  console.log(country.name.common + " :"+ country.region);
    // console.log(country.name.common + " :"+ country.subregion);
    
     for (var i=0 ; i< countries.length ;i++) {
-        console.log(countries[i].name.common + " :"+ countries[i].flag +" :"+ countries[i].region + " :"+ countries[i].subregion +" :"+ countries[i].population);
+        console.log(countries[i].name.common + ", FLAG  :"+ countries[i].flag +", REGION :"+ countries[i].region + ", SUBREGION :"+ countries[i].subregion +", POPULATION :"+ countries[i].population);
          
 
     }
